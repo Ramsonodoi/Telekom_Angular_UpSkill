3b4e8346487226781df95ca676be0e44b90b34ef
Ramson <ramson.odoi@amalitech.org>
2024-10-09 16:24:20 +0000

    test: add test case to test for response

--- src/app/services/auth.service.spec.ts
+++ src/app/services/auth.service.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed } from '@angular/core/testing';
+import { fakeAsync, TestBed, tick } from '@angular/core/testing';
 import { HttpTestingController, provideHttpClientTesting } from "@angular/common/http/testing";
 import { AuthService } from './auth.service';
 import { provideHttpClient } from '@angular/common/http';
@@ -24,15 +24,29 @@ describe('AuthService', () => {
     expect(service).toBeTruthy();
   });
 
-   it('should make a POST  request to register endpoint when valid email and password are provided',() => {
+   it('should make a POST  request to register endpoint when valid email and password are provided',fakeAsync(() => {
      const command: RegisterRequest = {
         email: 'michael.lawson@reqres.in',
         password: ' 4ahr8r;'
      }
 
-     service.register(command).subscribe()
+     let response: any
+     service.register(command).subscribe(res => {
+      response = res
+     })
      const request = httpTestController.expectOne(`https://reqres.in/api/register`)
 
      expect(request.request.method).toEqual('POST')
-   })
+     expect(request.request.body).toEqual(command)
+
+     request.flush({ id: 4, token: 'QpwL5tke4Pnpja7X4' });
+
+     tick();
+
+    expect(response).toEqual({ id: 4, token: 'QpwL5tke4Pnpja7X4' });
+
+    httpTestController.verify();
+    }))
+
+   
 });

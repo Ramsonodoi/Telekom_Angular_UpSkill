57bbf5db831d90908ae118b9ea689969b07b2496
Ramson <ramson.odoi@amalitech.org>
2024-10-10 16:17:11 +0000

    test: add test case to login a user

--- src/app/services/auth.service.spec.ts
+++ src/app/services/auth.service.spec.ts
@@ -1,8 +1,12 @@
+
+import { AuthResponse } from './../auth-response';
 import { fakeAsync, TestBed, tick } from '@angular/core/testing';
 import { HttpTestingController, provideHttpClientTesting } from "@angular/common/http/testing";
 import { AuthService } from './auth.service';
 import { provideHttpClient } from '@angular/common/http';
 import { RegisterRequest } from '../register-request';
+import { LoginRequest } from '../login-request';
+
 
 
 
@@ -48,5 +52,45 @@ describe('AuthService', () => {
     httpTestController.verify();
     }))
 
+   it('should handle successful login', fakeAsync(() => {
+
+    const command: LoginRequest = {
+      email: 'michael.lawson@reqres.in',
+      password: ' 4ahr8r;'
+   }
+     
+     const mockResponse: AuthResponse = {
+       token: 'QpwL5tke4Pnpja7X4'
+      }
+
+
+      let response: AuthResponse | undefined
+
    
+   service.login(command).subscribe(
+        {
+          next: (res: AuthResponse) => {
+            response = res
+          },
+
+          error: () => {
+             fail('should not reach the error block for this test')
+          }
+        } 
+   )
+   const request = httpTestController.expectOne(`https://reqres.in/api/login`)
+
+   expect(request.request.method).toEqual('POST')
+   expect(request.request.body).toEqual(command)
+
+   request.flush(mockResponse)
+
+   tick()
+
+   expect(response).toEqual(mockResponse)
+   httpTestController.verify()
+   }))
+
+
+ 
 });

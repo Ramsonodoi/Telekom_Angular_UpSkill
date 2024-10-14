84fcbf87fb9de1269d4e33cf4935cf22b56b6fe9
Ramson <ramson.odoi@amalitech.org>
2024-10-14 09:42:38 +0000

    test: add test case to actually test changing the color

--- src/app/shared/changecolor.directive.spec.ts
+++ src/app/shared/changecolor.directive.spec.ts
@@ -1,10 +1,45 @@
+import { Colors } from './../../../node_modules/picocolors/types';
 import { ElementRef } from '@angular/core';
 import { ChangecolorDirective } from './changecolor.directive';
 
+
 describe('ChangecolorDirective', () => {
+
+  let directive: ChangecolorDirective
+
+  let mockElementRef: ElementRef
+
+  beforeEach(() => {
+    mockElementRef = new ElementRef({
+      style: {}
+    })
+    directive  = new ChangecolorDirective(mockElementRef)
+  })
+
+
   it('should create an instance', () => {
-    const mockElementRef = new ElementRef(null)
-    const directive = new ChangecolorDirective(mockElementRef);
     expect(directive).toBeTruthy();
   });
+
+
+  it('should set default color to green', () => {
+    directive.ngOnChanges()
+    expect(mockElementRef.nativeElement.style.color).toBe('green')
+  })
+
+  it ('should use default color when input is empty string', () => {
+    directive.companyName = ''
+    directive.ngOnChanges()
+    expect(mockElementRef.nativeElement.style.color).toBe('green')
+  })
+
+  it('should update color when input changes', () => {
+    directive.companyName = 'red'
+    directive.ngOnChanges()
+    expect(mockElementRef.nativeElement.style.color).toBe('red')
+
+    directive.companyName = 'yellow'
+    directive.ngOnChanges()
+    expect(mockElementRef.nativeElement.style.color).toBe('yellow')
+  })
 });
